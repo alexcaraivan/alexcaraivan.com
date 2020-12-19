66cd8bd3554a5c003658020b08623ed82f2e6b62
Alex Caraivan <alex.c@adoreme.com>
2020-12-19 17:35:32 +0200

    changed favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/favicon.svg" />
+        </Head>
         <body className="bg-white text-black dark:bg-black dark:text-white">
           <Main />
           <NextScript />

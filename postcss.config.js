35b172d5318ae265d802502f992f2b237d56d16e
Alex Caraivan <alex.c@adoreme.com>
2021-05-03 19:19:38 +0300

    found the culprit

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,7 @@ const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
     // Specify the paths to all of the template files
-    content: [
-      './pages/**/*.{js,jsx,ts,tsx}',
-      './components/**/*.{js,jsx,ts,tsx}',
-    ],
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
     // This is the function used to extract class names from the templates
     defaultExtractor: (content) => {
       // Capture as liberally as possible, including things like `h-(screen-1.5)`
@@ -17,9 +14,5 @@ const purgecss = [
   },
 ];
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    process.env.NODE_ENV === 'production' ? purgecss : undefined,
-    'postcss-preset-env',
-  ],
+  plugins: ['tailwindcss', 'postcss-preset-env'],
 };
